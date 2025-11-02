201ebb072a468086ac2147ecf3d346648eef7ce1
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-02 09:50:11 -0500

    Update ViewController.swift

--- Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
+++ Unit1/Lesson1_7/lab/FirstTimeDebugging/FirstTimeDebugging/ViewController.swift
@@ -2,29 +2,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad( {
+    override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         var sample = "sample"
         print(sample)
-        
+
         if false {
             print("Will this line of code ever be reached?")
-            someMetho()
+            someMethod()
         }
-        
-        
     }
-    
-    func someMethod() {
 
+    func someMethod() {
+        // You can add functionality here
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
