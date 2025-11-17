0d48ad4f102f08d7c661266c5096c50e3fc4016d
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-16 19:32:47 -0500

    Update Contents.swift

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/2. App Exercise - A Functioning App.xcplaygroundpage/Contents.swift
@@ -1,18 +1,33 @@
-/*:
-## App Exercise - A Functioning App
- 
- >These exercises reinforce Swift concepts in the context of a fitness tracking app.
- 
- As you may have guessed, functions are key to making your app work. For example, in every exercise dealing with step count until now, you have simply assigned a number of steps to a `steps` variable. This isn't very realistic seeing as the number of steps you take increments one at a time and continues changing throughout the day.
- 
- A reoccurring process like this is a perfect candidate for a function. Write a function called `incrementSteps` after the declaration of `steps` below that will increment `steps` by one and then print its value. Call the function multiple times and observe the printouts.
- */
 var steps = 0
 
-//:  Similarly, if you want to regularly provide progress updates to your user, you can put your control flow statements that check on progress into a function. Write a function called `progressUpdate` after the declaration of `goal` below. The function should print "You're off to a good start." if `steps` is less than 10% of `goal`, "You're almost halfway there!" if `steps` is less than half of `goal`, "You're over halfway there!" if `steps` is less than 90% of `goal`, "You're almost there!" if `steps` is less than `goal`, and "You beat your goal!" otherwise. Call the function and observe the printout.
-let goal = 10000 
+// Function to increment steps by 1 and print the new value
+func incrementSteps() {
+    steps += 1
+    print("Steps: \(steps)")
+}
+
+// Call the function multiple times
+incrementSteps()
+incrementSteps()
+incrementSteps()
 
 
-/*:
-[Previous](@previous)  |  page 2 of 6  |  [Next: Exercise - Parameters and Argument Labels](@next)
- */
+let goal = 10000
+
+// Function to provide progress updates based on steps vs. goal
+func progressUpdate() {
+    if steps < goal / 10 {
+        print("You're off to a good start.")
+    } else if steps < goal / 2 {
+        print("You're almost halfway there!")
+    } else if steps < (goal * 9) / 10 {
+        print("You're over halfway there!")
+    } else if steps < goal {
+        print("You're almost there!")
+    } else {
+        print("You beat your goal!")
+    }
+}
+
+// Call the function to see the progress update
+progressUpdate()
