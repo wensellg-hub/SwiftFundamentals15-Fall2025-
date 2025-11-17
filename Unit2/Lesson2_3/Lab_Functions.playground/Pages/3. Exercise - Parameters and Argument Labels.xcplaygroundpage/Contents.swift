a2aa3a33b8d8209b8f123c3c168cf9a124d928cb
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-16 19:35:22 -0500

    Update Contents.swift

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/3. Exercise - Parameters and Argument Labels.xcplaygroundpage/Contents.swift
@@ -1,16 +1,27 @@
-/*:
-## Exercise - Parameters and Argument Labels
- 
- Write a new introduction function called `introduction`. It should take two `String` parameters, `name` and `home`, and one `Int` parameter, `age`. The function should print a brief introduction. I.e. if "Jenny," "California," and 32 were passed into the function, it might print "Jenny, 32, is from California." Call the function and observe the printout.
- */
+// Introduction function
+func introduction(name: String, home: String, age: Int) {
+    print("\(name), \(age), is from \(home).")
+}
+
+// Call the function
+introduction(name: "Jenny", home: "California", age: 32)
 
 
-//:  Write a function called `almostAddition` that takes two `Int` arguments. The first argument should not require an argument label. The function should add the two arguments together, subtract 2, then print the result. Call the function and observe the printout.
+// almostAddition function
+func almostAddition(_ first: Int, second: Int) {
+    let result = first + second - 2
+    print("Result: \(result)")
+}
+
+// Call the function
+almostAddition(10, second: 5)
 
 
-//:  Write a function called `multiply` that takes two `Double` arguments. The function should multiply the two arguments and print the result. The first argument should not require a label, and the second argument should have an external label, `by`, that differs from the internal label. Call the function and observe the printout.
+// multiply function
+func multiply(_ first: Double, by second: Double) {
+    let result = first * second
+    print("Result: \(result)")
+}
 
-
-/*:
-[Previous](@previous)  |  page 3 of 6  |  [Next: App Exercise - Progress Updates](@next)
- */
+// Call the function
+multiply(3.0, by: 4.5)
