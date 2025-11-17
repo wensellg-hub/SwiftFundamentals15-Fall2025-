619b44c6e493ff2a656df943479fe5f61dfb4362
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-16 21:26:53 -0500

    Update Contents.swift

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/2. App Exercise - Workout Tracking.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/2. App Exercise - Workout Tracking.xcplaygroundpage/Contents.swift
@@ -1,20 +1,20 @@
-/*:
-## App Exercise - Workout Tracking
- 
- >These exercises reinforce Swift concepts in the context of a fitness tracking app.
- 
- Your fitness tracking app wouldn't be much of a fitness tracker if it couldn't help users track their workouts. In order to track a user's run, you'll need to have some kind of data structure that can hold information about the workout. For the sake of simplicity, you'll focus specifically on running workouts.
- 
- Create a `RunningWorkout` struct. It should have variables properties for `distance`, `time`, and `elevation`. All three properties should have default values of 0.0.
- */
+// Define the RunningWorkout struct
+struct RunningWorkout {
+    var distance: Double = 0.0
+    var time: Double = 0.0
+    var elevation: Double = 0.0
+}
 
+// Create a variable instance of RunningWorkout
+var firstRun = RunningWorkout()
 
-//:  Create a variable instance of `RunningWorkout` called `firstRun` without supplying any arguments. Print out all three properties of `firstRun`. This is a good example of when using default values is appropriate, seeing as all running workouts start with a distance, time, and elevation change of 0.
+// Print default values
+print("Distance: \(firstRun.distance) meters, Time: \(firstRun.time) minutes, Elevation: \(firstRun.elevation) meters")
 
+// Update values to reflect the run
+firstRun.distance = 2396
+firstRun.time = 15.3
+firstRun.elevation = 94
 
-//:  Now imagine that throughout the course of the run, you go a distance of 2,396 meters in 15.3 minutes, and gain 94 meters of elevation. Update the values of `firstRun`'s properties accordingly. Print a statement about your run using the values of each property.
-
-
-/*:
-[Previous](@previous)  |  page 2 of 10  |  [Next: Exercise - Memberwise and Custom Initializers](@next)
- */
+// Print a statement about the run
+print("I ran \(firstRun.distance) meters in \(firstRun.time) minutes and gained \(firstRun.elevation) meters of elevation.")
