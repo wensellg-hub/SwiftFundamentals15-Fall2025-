13a1722122998a4e6f95e9db3b50bc33c3345e3b
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-16 19:24:11 -0500

    Update Contents.swift

--- Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_3/Lab_Functions.playground/Pages/1. Exercise - Create Functions.xcplaygroundpage/Contents.swift
@@ -1,14 +1,33 @@
-/*:
-## Exercise - Create Functions
+// Function that prints a brief introduction
+func introduceMyself() {
+    print("Hi, my name is Grant. I'm an administrative and customer service professional currently studying at Durham Technical Community College, and I enjoy learning programming and exploring new technologies.")
+}
 
- Write a function called `introduceMyself` that prints a brief introduction of yourself. Call the function and observe the printout.
- */
-
-
-//:  Write a function called `magicEightBall` that generates a random number and then uses either a switch statement or if-else-if statements to print different responses based on the random number generated. `let randomNum = Int.random(in: 0...4)` will generate a random number from 0 to 4, after which you can print different phrases corresponding to the number generated. Call the function multiple times and observe the different printouts.
-import Foundation
+// Call the function
+introduceMyself()
 
 
-/*:
-page 1 of 6  |  [Next: App Exercise - A Functioning App](@next)
- */
+// Function that simulates a Magic Eight Ball
+func magicEightBall() {
+    let randomNum = Int.random(in: 0...4)
+    
+    switch randomNum {
+    case 0:
+        print("Yes, definitely!")
+    case 1:
+        print("Ask again later.")
+    case 2:
+        print("No way!")
+    case 3:
+        print("It’s possible, but uncertain.")
+    case 4:
+        print("Absolutely!")
+    default:
+        print("Error: unexpected number.")
+    }
+}
+
+// Call the function multiple times to see different outputs
+magicEightBall()
+magicEightBall()
+magicEightBall()
