394f090786fec06e92ac2d7702a1587a769ee27d
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-16 21:37:25 -0500

    Update Contents.swift

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/6. App Exercise - Workout Functions.xcplaygroundpage/Contents.swift
@@ -1,26 +1,34 @@
-/*:
-## App Exercise - Workout Functions
- 
- >These exercises reinforce Swift concepts in the context of a fitness tracking app.
- 
- A `RunningWorkout` struct has been created for you below. Add a method on `RunningWorkout` called `postWorkoutStats` that prints out the details of the run. Then create an instance of `RunningWorkout` and call `postWorkoutStats()`.
- */
+// Extend RunningWorkout with a method
 struct RunningWorkout {
     var distance: Double
     var time: Double
     var elevation: Double
     
+    // Method to print workout stats
+    func postWorkoutStats() {
+        print("You ran \(distance) meters in \(time) minutes with an elevation gain of \(elevation) meters.")
+    }
 }
 
+// Create an instance and call the method
+let firstRun = RunningWorkout(distance: 2396, time: 15.3, elevation: 94)
+firstRun.postWorkoutStats()
 
-//:  A `Steps` struct has been created for you below, representing the day's step-tracking data. It has the goal number of steps for the day and the number of steps taken so far. Create a method on `Steps` called `takeStep` that increments the value of `steps` by one. Then create an instance of `Steps` and call `takeStep()`. Print the value of the instance's `steps` property before and after the method call.
+
+// Extend Steps with a mutating method
 struct Steps {
     var steps: Int
     var goal: Int
     
+    // Method to increment steps
+    mutating func takeStep() {
+        steps += 1
+    }
 }
 
+// Create an instance and test the method
+var todaySteps = Steps(steps: 9999, goal: 10000)
 
-/*:
-[Previous](@previous)  |  page 6 of 10  |  [Next: Exercise - Computed Properties and Property Observers](@next)
- */
+print("Steps before: \(todaySteps.steps)")
+todaySteps.takeStep()
+print("Steps after: \(todaySteps.steps)")
