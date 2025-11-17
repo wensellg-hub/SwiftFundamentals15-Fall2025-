549dc696a7a8d65e3ec79570ca5b21f221612091
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-16 21:42:22 -0500

    Update Contents.swift

--- Unit2/Lesson2_4/Lab_Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_4/Lab_Structures.playground/Pages/8. App Exercise - Mile Times and Congratulations.xcplaygroundpage/Contents.swift
@@ -1,27 +1,30 @@
-/*:
-## App Exercise - Mile Times and Congratulations
- 
- >These exercises reinforce Swift concepts in the context of a fitness tracking app.
- 
- The `RunningWorkout` struct below holds information about your users' running workouts. However, you decide to add information about average mile time. Add a computed property called `averageMileTime` that uses `distance` and `time` to compute the user's average mile time. Assume that `distance` is in meters and 1600 meters is a mile.
- 
- Create an instance of `RunningWorkout` and print the `averageMileTime` property. Check that it works properly.
- */
+// RunningWorkout struct with computed property
 struct RunningWorkout {
     var distance: Double
     var time: Double
     var elevation: Double
     
+    // Computed property for average mile time
+    var averageMileTime: Double {
+        let miles = distance / 1600.0
+        return time / miles
+    }
 }
 
+// Create an instance and print averageMileTime
+let workout = RunningWorkout(distance: 3200, time: 20.0, elevation: 50)
+print("Average mile time: \(workout.averageMileTime) minutes")
 
-/*:
- In other app exercises, you've provided encouraging messages to the user based on how many steps they've completed. A great place to check whether or not you should display something to the user is in a property observer.
- 
- In the `Steps` struct below, add a `willSet` to the `steps` property that will check if the new value is equal to `goal`, and if it is, prints a congratulatory message. Create an instance of `Steps` where `steps` is 9999 and `goal` is 10000, then call `takeStep()` and see if your message is printed to the console.
- */
+
+// Steps struct with property observer
 struct Steps {
-    var steps: Int
+    var steps: Int {
+        willSet {
+            if newValue == goal {
+                print("Congratulations! You've reached your goal of \(goal) steps!")
+            }
+        }
+    }
     var goal: Int
     
     mutating func takeStep() {
@@ -29,7 +32,9 @@ struct Steps {
     }
 }
 
+// Create an instance and test property observer
+var todaySteps = Steps(steps: 9999, goal: 10000)
 
-/*:
-[Previous](@previous)  |  page 8 of 10  |  [Next: Exercise - Type Properties and Methods](@next)
- */
+print("Steps before: \(todaySteps.steps)")
+todaySteps.takeStep() // should trigger congratulatory message
+print("Steps after: \(todaySteps.steps)")
