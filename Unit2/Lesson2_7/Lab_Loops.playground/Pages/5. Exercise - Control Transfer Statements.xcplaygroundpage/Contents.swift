7d9bc2105d89926e2e51756d86867511bfbb5607
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-20 18:06:32 -0500

    Update Contents.swift

--- Unit2/Lesson2_7/Lab_Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_7/Lab_Loops.playground/Pages/5. Exercise - Control Transfer Statements.xcplaygroundpage/Contents.swift
@@ -1,14 +1,25 @@
-/*:
-## Exercise - Control Transfer Statements
- 
- Create a for-in loop that will loop through `alphabet`. Inside the loop, print every other letter by continuing to the next iteration if you are on a letter you do not wish to print. (Hint: You can use the `isMultiple(of:)` method on `Int` to only print even indexed characters).
- */
+// Part 1: Loop through alphabet and print every other letter
 let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+for (index, letter) in alphabet.enumerated() {
+    // Skip odd indices, only print even ones
+    if !index.isMultiple(of: 2) {
+        continue
+    }
+    print(letter)
+}
 
-//:  Create a `[String: String]` dictionary where the keys are names of states and the values are their capitals. Include at least three key/value pairs in your collection, with one of them being your home state. Now loop through this dictionary again, printing out the keys and values in a sentence, but add an if statement that will check if the current iteration is your home state. If it is, print("I found my home!") and break out of the loop.
+// Part 2: Dictionary of states and capitals
+let stateCapitals: [String: String] = [
+    "North Carolina": "Raleigh",
+    "California": "Sacramento",
+    "Texas": "Austin"
+]
 
-
-/*:
-[Previous](@previous)  |  page 5 of 6  |  [Next: App Exercise - Finding Movements](@next)
- */
+for (state, capital) in stateCapitals {
+    print("The capital of \(state) is \(capital).")
+    if state == "North Carolina" {
+        print("I found my home!")
+        break
+    }
+}
