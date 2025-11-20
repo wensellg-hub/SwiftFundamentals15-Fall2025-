2b84fbd7ebbfcd43867dd3d2ac2370eb1916c68c
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-19 21:34:31 -0500

    Update Contents.swift

--- Unit2/Lesson2_6/Lab_Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_6/Lab_Collections.playground/Pages/2. App Exercise - Activity Challenge.xcplaygroundpage/Contents.swift
@@ -1,26 +1,28 @@
-/*:
-## App Exercise - Activity Challenge
- 
- >These exercises reinforce Swift concepts in the context of a fitness tracking app.
- 
- Your fitness tracking app shows users a list of possible challenges, grouped by activity type (i.e. walking challenges, running challenges, calisthenics challenges, weightlifting challenges, etc.) A challenge could be as simple as "Walk 3 miles a day" or as intense as "Run 5 times a week." 
- 
- Using arrays of type `String`, create at least two lists, one for walking challenges, and one for running challenges. Each should have at least two challenges and should be initialized using an array literal. Feel free to create more lists for different activities.
- */
+// Step 1: Create lists of challenges
+let walkingChallenges: [String] = ["Walk 3 miles a day", "Walk 10,000 steps daily"]
+let runningChallenges: [String] = ["Run 5 times a week", "Run a half marathon"]
 
+// You can add more if you like, e.g.:
+let weightliftingChallenges: [String] = ["Lift weights 3 times a week", "Bench press your bodyweight"]
 
-//:  In your app you want to show all of these lists on the same screen grouped into sections. Create a `challenges` array that holds each of the lists you have created (it will be an array of arrays). Using `challenges`, print the first element in the second challenge list.
+// Step 2: Group them into an array of arrays
+var challenges: [[String]] = [walkingChallenges, runningChallenges, weightliftingChallenges]
 
+// Step 3: Print the first element in the second challenge list
+print(challenges[1][0])   // "Run 5 times a week"
 
-//:  All of the challenges will reset at the end of the month. Use the `removeAll` to remove everything from `challenges`. Print `challenges`.
+// Step 4: Reset all challenges at the end of the month
+challenges.removeAll()
+print(challenges)         // []
 
+// Step 5: Create a new array for committed challenges
+var committedChallenges: [String] = []   // could also start with some items
 
-//:  Create a new array of type `String` that will represent challenges a user has committed to instead of available challenges. It can be an empty array or have a few items in it.
-
-
-//:  Write an if statement that will use `isEmpty` to check if there is anything in the array. If there is not, print a statement asking the user to commit to a challenge. Add an else-if statement that will print "The challenge you have chosen is <INSERT CHOSEN CHALLENGE>" if the array count is exactly 1. Then add an else statement that will print "You have chosen multiple challenges."
-
-
-/*:
-[Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Dictionaries](@next)
- */
+// Step 6: Use if/else-if/else with isEmpty and count
+if committedChallenges.isEmpty {
+    print("Please commit to a challenge.")
+} else if committedChallenges.count == 1 {
+    print("The challenge you have chosen is \(committedChallenges[0])")
+} else {
+    print("You have chosen multiple challenges.")
+}
