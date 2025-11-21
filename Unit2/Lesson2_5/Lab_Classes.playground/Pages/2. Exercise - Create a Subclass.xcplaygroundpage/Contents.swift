9e66ca3deb490d4d567b78ac7ad0823d45abb2b6
wensellg-hub <wensellg9676@durhamtech.edu>
2025-11-21 14:26:39 -0500

    Update Contents.swift

--- Unit2/Lesson2_5/Lab_Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
+++ Unit2/Lesson2_5/Lab_Classes.playground/Pages/2. Exercise - Create a Subclass.xcplaygroundpage/Contents.swift
@@ -1,8 +1,4 @@
-/*:
-## Exercise - Create a Subclass
-
- - Note: The exercises below are based on a game where a spaceship avoids obstacles in space. The ship is positioned at the bottom of a coordinate system and can only move left and right while obstacles "fall" from top to bottom. Throughout the exercises, you'll create classes to represent different types of spaceships that can be used in the game. The base class `Spaceship` has been provided for you below.
- */
+// Base class provided
 class Spaceship {
     var name: String = ""
     var health = 100
@@ -23,18 +19,39 @@ class Spaceship {
         }
     }
 }
-//:  Define a new class `Fighter` that inherits from `Spaceship`. Add a variable property `weapon` that defaults to an empty string and a variable property `remainingFirePower` that defaults to 5.
 
+// Step 1: Define Fighter subclass
+class Fighter: Spaceship {
+    var weapon: String = ""
+    var remainingFirePower: Int = 5
+    
+    func fire() {
+        if remainingFirePower > 0 {
+            remainingFirePower -= 1
+            print("Fired \(weapon)! Remaining fire power: \(remainingFirePower)")
+        } else {
+            print("You have no more fire power.")
+        }
+    }
+}
 
-//:  Create a new instance of `Fighter` called `destroyer`. A `Fighter` will be able to shoot incoming objects to avoid colliding with them. After initialization, set `weapon` to "Laser" and `remainingFirePower` to 10. Note that since `Fighter` inherits from `Spaceship`, it also has properties for `name`, `health`, and `position`, and has methods for `moveLeft()`, `moveRight()`, and `wasHit()` even though you did not specifically add them to the declaration of `Fighter`. Knowing that, set `name` to "Destroyer," print `position`, then call `moveRight()` and print `position` again.
+// Step 2: Create instance of Fighter
+let destroyer = Fighter()
+destroyer.weapon = "Laser"
+destroyer.remainingFirePower = 10
+destroyer.name = "Destroyer"
 
+// Demonstrate inherited properties and methods
+print("Initial position: \(destroyer.position)")
+destroyer.moveRight()
+print("Position after moving right: \(destroyer.position)")
 
-//:  Try to print `weapon` on `falcon`. Why doesn't this work? Provide your answer in a comment or a print statement below, and remove any code you added that doesn't compile.
+// Step 3: Why can't we print weapon on falcon?
+let falcon = Spaceship()
+// print(falcon.weapon) ❌ This doesn't compile because `Spaceship` has no `weapon` property.
+print("Falcon is a Spaceship, not a Fighter, so it has no weapon property.")
 
-
-//:  Add a method to `fighter` called `fire()`. This should check to see if `remainingFirePower` is greater than 0, and if so, should decrement `remainingFirePower` by one. If `remainingFirePower` is not greater than 0, print "You have no more fire power." Call `fire()` on `destroyer` a few times and print `remainingFirePower` after each method call.
-
-
-/*:
-[Previous](@previous)  |  page 2 of 4  |  [Next: Exercise - Override Methods and Properties](@next)
- */
+// Step 4: Call fire() on destroyer a few times
+destroyer.fire()  // Remaining fire power: 9
+destroyer.fire()  // Remaining fire power: 8
+destroyer.fire()  // Remaining fire power: 7
